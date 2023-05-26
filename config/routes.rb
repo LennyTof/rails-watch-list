edcfba4d3afa718d1c63858304abeee7c7e95c07
Lenny Sentuc <lenny.sentuc@hotmail.fr>
2023-05-26 11:48:17 +0200

    Add delete button and image in list show

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'lists#index'
+  resources :lists, only: %i[index new show create] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: %i[destroy]
 end
